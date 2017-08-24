39f700afef33e9f04387ff52982ccfc5ba04ecf0
ShayneGao <gwlj@outlook.com>
2017-08-24 15:04:22 +0800

    change shell

--- bin/run.sh
+++ bin/run.sh
@@ -1,7 +1,7 @@
 #rm -f ../output/*
-mkdir ../output
-mkdir ../log
 ls_date=`date +%Y%m`
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+mkdir ${DIR}/../output
+mkdir ${DIR}/../log
 nohup scrapy crawl wm_spider -o ${DIR}/../output/${ls_date}.csv -s LOG_FILE=${DIR}/../log/${ls_date}.log 
 #sz ../output/${ls_date}.csv 
